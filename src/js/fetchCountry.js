5c8fdf8441d8d1d609378627d22ea37c62cc3072
kuntuganova <elya.kuntuganova@gmail.com>
2023-03-15 21:04:46 +0600

    fetchCountry commit

---
+++ src/js/fetchCountry.js
@@ -0,0 +1,10 @@
+export function fetchCountry(name) {
+  return fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,languages,flags`
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json;
+  });
+}
