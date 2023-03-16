a6f0a24446ef5ad5c147a352e1aff84cac9444e4
kuntuganova <elya.kuntuganova@gmail.com>
2023-03-16 21:11:47 +0600

    updated commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import './css/styles.css';
-
+import { refs } from './js/refs';
+import { fetchCountry } from './js/fetchCountry';
+import { countryCardTemplate, countryListTemplate } from './js/markupTemplate';
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const DEBOUNCE_DELAY = 300;
